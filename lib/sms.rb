b4db3e0fded58028330e95833d23ac1512d2c550
PeoneEr <peoneerko@yandex.ru>
2015-01-28 11:03:20 +0600

    some code refactor

--- lib/sms.rb
+++ lib/sms.rb
@@ -1,6 +1,9 @@
 class Sms
   def send(message)
-    message = MainsmsApi::Message.new(sender: 'ZnaiGorod', message: message, recipients: [Settings['sms_recipient.phone']])
+    message = MainsmsApi::Message.new(sender: 'ZnaiGorod', message: message, recipients: [Settings['sms_recipient.phone']]) if Rails.env.production?
+
+    message = MainsmsApi::Message.new(sender: 'ZnaiGorod', message: message, recipients: [Settings['sms_recipient.phone']], test: 1) if Rails.env.development?
+
     message.deliver
   end
 end
